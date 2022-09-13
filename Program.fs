97e63cbd0a641feedfd2650b88807f4cf97d9563
Gustavo Hidalgo <Gustavo.Hidalgo@newellco.com>
2022-09-13 16:22:21 -0400

    save

--- Program.fs
+++ Program.fs
@@ -35,6 +35,23 @@ let rec takeWhile fn test i =
         | false ->
             ()
     }
+
+// we can approximate with an order or magnitude check? if n = v * 10^P we can go up to 10^(P/2 + 1), + 1 to over count.
+// >> is a function composition operator!
+// >>> is the right bit shift operator!
+// https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/symbol-and-operator-reference/#function-symbols-and-operators
+let approximateSqrt (n : bigint) : bigint = n >>> (int(n.GetBitLength()) / 2 - 1)
+
+// Does not include 1
+let rec primeFactors (n : bigint) =
+    let hi = approximateSqrt n
+    let rec p (i : bigint) =
+        match n % i with
+        | zero when zero = 0 -> i :: primeFactors (n / i)
+        | remainder when i < hi -> p (i + bigint 1)
+        | _ -> [n]
+    p 2
+
 let fibs =
     takeWhile fib (fun n -> n < 4_000_000) 0
     |> Seq.filter (fun n -> n % (bigint 2) = (bigint 0))
